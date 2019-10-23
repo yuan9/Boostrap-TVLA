c4c904806b8e05f4f888cdf79e0fd05bae9a0f0a
Yuan <yuan9@vt.edu>
2019-10-22 22:29:23 -0400

    trace reading working

--- hw-AES-Protected/TVLA_resolution.py
+++ hw-AES-Protected/TVLA_resolution.py
@@ -29,17 +29,48 @@ import util.dwdb_reader as io
 import util.tests as tests
 
 
-tracenum = 2
-
+tracenum = 5
+#sample_start = 40
+#sample_end = 180
+sample_start = 0
+sample_end = 3
 dsr = io.dwdb_reader('/Users/yaoyuan/Desktop/Boostrap-TVLA/hw-AES-Protected/RawTraces.dwdb', '/Users/yaoyuan/Desktop/Boostrap-TVLA/hw-AES-Protected/')
-data_batch, meta_batch = dsr.read_batch(tracenum, 40, 200)
+data_batch, meta_batch = dsr.read_batch(tracenum, sample_start, sample_end)
 data_np = np.asarray(data_batch)
 
 #processing of classifiers
-classifiers = [m['classifiers'].strip('{}') for m in meta_batch]
-#classifiers = np.asarray(classifiers) # 2D numpy array of classifier
+classifiers = [s.split('=')[1] for m in meta_batch for s in m['other'].split() if s.startswith('s=')]
+classifiers= np.asarray(classifiers) # 2D numpy array of classifier
 
-print (meta_batch)
-print (data_batch)
-plt.plot(data_batch[0])
+# initializing the rand and fix dataset
+rand = []
+fix = []
+for i in range(0, tracenum):
+	if classifiers[i] == '1':
+		rand.append(data_np[i])
+	if classifiers[i] == '0':
+		fix.append(data_np[i])
+print("rand:{}".format(rand))
+print("fix:{}".format(fix))
+
+rand_np = np.asarray(rand)
+rand_trans =  rand_np.T
+print (rand_trans)
+
+fix_np = np.asarray(fix)
+fix_trans =  fix_np.T
+print (fix_trans)
+
+# t-statistics
+t_value =[]
+for i in range(0, sample_end - sample_start):
+	t, p = stats.ttest_ind(rand_trans[i], fix_trans[i], equal_var = False)
+	t_value.append(t)
+# data = np.ones_like(data_np[0])
+# data_label = np.ones_like(classifiers[0])
+# print (data)
+# print (data_label)
+#print (classifiers)
+#print (data_batch[1])
+plt.plot(t_value)
 plt.show()
