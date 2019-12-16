10058f43a2c3b5e4c3e2929b0994a384f1d88c3c
anton <anton@a.net>
2019-12-15 18:00:29 -0800

    Added another way to import betainc

--- hw-AES-Protected/util/func.py
+++ hw-AES-Protected/util/func.py
@@ -1,15 +1,31 @@
 import numpy as np
 
-from scipy.special import kolmogorov, smirnov
+from scipy.special import kolmogorov, smirnov, betainc
 from scipy.stats.distributions import ksone, kstwobign
 
 from scipy.stats.stats import _betai
 
+dpawstools = False
+try:
+  import DpawsTools
+  dpawstools = True
+except ImportError:
+  pass
+
+def dpa_ttest(traces, classifiers):
+  pass
+
+
 # Converting correlation to pvalue
+def betai(a, b, x):
+  x = np.asarray(x)
+  x = np.where(x < 1.0, x, 1.0)  # if x > 1 then return 1.0
+  return betainc(a, b, x)
+
 def r2pv(r, n):
   df = n - 2
   tsq = r**2 * (df / ((1.0 - r) * (1.0 + r)))
-  return _betai(0.5*df, 0.5, df/(df+tsq))
+  return betai(0.5*df, 0.5, df/(df+tsq))
 
 def kstest(rvs, cdf):
   N = len(rvs)
