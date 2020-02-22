9ae5d052557dfc081b8be11b3c3c6727cb209c52
anton <anton@a.net>
2020-02-21 18:49:38 -0800

    Improved boot visualization, sample point handling, added pv visualization

--- falsepositive/fp_DataGenerator.py
+++ falsepositive/fp_DataGenerator.py
@@ -63,7 +63,7 @@ while True:
   y_volts = x_volts + noise_volts
   rand1_noise = y_volts
   # print ("rand1_noise:{}".format(rand1_noise[0:10]))
-  
+
   #----------------------------------------------#
   # generate a random list
   #----------------------------------------------#
@@ -72,7 +72,7 @@ while True:
   x_volts_orig = hw_randlist
   x_volts = x_volts_orig + offset
 
-  # Calculate signal power and convert to dB 
+  # Calculate signal power and convert to dB
   mean_noise = 0
   sigma_noise = math.sqrt(np.var(x_volts)/target_snr_db)
   noise_volts = np.random.normal(mean_noise, sigma_noise, len(x_volts))
@@ -90,20 +90,20 @@ while True:
   x_volts_orig = hw_randlist
   x_volts = x_volts_orig + offset
 
-  # Calculate signal power and convert to dB 
+  # Calculate signal power and convert to dB
   mean_noise = 0
   sigma_noise = math.sqrt(np.var(x_volts)/target_snr_db)
   noise_volts = np.random.normal(mean_noise, sigma_noise, len(x_volts))
 
   # Noise up the original signal
   y_volts = x_volts + noise_volts
-  fix = y_volts          
-  
+  fix = y_volts
+
   t,  p  = stats.ttest_ind(rand1_noise, rand2_noise, equal_var = False)
   t2, p2 = stats.ttest_ind(rand1_noise, fix,         equal_var = False)
 
   #generate true positive case with small t value
-  if abs(t2) > 4.5 and abs(t2) < 6 and counter2 < counter:
+  if abs(t2) > 5 and abs(t2) < 7 and counter2 < counter:
     counter2 = counter2 + 1
     print("find tp {}".format(counter2))
     #np.savetxt(result_dir+'/{}Rand1Noise_{}traces_{}SNR_f{}.txt'.format(abs(t), trace_num,target_snr_db,fix_value),rand1_noise,fmt = '%s', delimiter=',')
@@ -112,7 +112,7 @@ while True:
     np.savetxt(result_dir_tp+'/{}-{}fix_{}traces_{}SNR_f{}.txt'.format(counter, script_no, trace_num,target_snr_db,fix_value),fix,fmt = '%s', delimiter=',')
 
   #generate false positive case
-  if abs(t) > 4.5:
+  if abs(t) > 5:
     counter = counter + 1
     print("find fp {}".format(counter))
     #np.savetxt(result_dir+'/{}Rand1Noise_{}traces_{}SNR_f{}.txt'.format(abs(t), trace_num,target_snr_db,fix_value),rand1_noise,fmt = '%s', delimiter=',')
@@ -120,5 +120,5 @@ while True:
     np.savetxt(result_dir_fp+'/{}-{}Rand1Noise_{}traces_{}SNR_f{}.txt'.format(counter, script_no, trace_num,target_snr_db,fix_value),rand1_noise,fmt = '%s', delimiter=',')
     np.savetxt(result_dir_fp+'/{}-{}Rand2Noise_{}traces_{}SNR_f{}.txt'.format(counter, script_no, trace_num,target_snr_db,fix_value),rand2_noise,fmt = '%s', delimiter=',')
 
-  # print('.', end='')
+  #print('.', end='')
 
