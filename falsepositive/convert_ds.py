b599af854c331e073cd56baf263fa333f93b26fb
anton <anton@a.net>
2020-03-03 10:06:07 -0800

    Added more true positives. Improved conversion

--- falsepositive/convert_ds.py
+++ falsepositive/convert_ds.py
@@ -14,20 +14,33 @@ import util.dwdb_reader as io
 import util.func as f
 
 
-ds_dir = 'TP_set.2'
-dsw = io.dwdb_writer(r'TP_db\log.dwdb', '', 'TP_db')
+tp_dir, fp_dir = 'TP_set', 'FP_set'
+dsw_tp = io.dwdb_writer(r'TP_db.new\log.dwdb', '', 'TP_db.new')
+dsw_fp = io.dwdb_writer(r'FP_db.new\log.dwdb', '', 'FP_db.new')
 
-fp = re.compile(r'(\d+)-(\d+)Rand(\d)Noise')
-tp = re.compile(r'(\d+)-(\d+)(tp|fix)_1000tr')
+fpre = re.compile(r'(\d+)-(\d+)Rand(\d)Noise')
+tpre = re.compile(r'(\d+)-(\d+)(tp|fix)_1000tr')
 
-for dirName, subdirList, fileList in os.walk(ds_dir):
+for dirName, subdirList, fileList in os.walk(tp_dir):
   print('Processing directory: {}'.format(dirName))
   for fname in fileList:
-    m = tp.search(fname)
+    m = tpre.search(fname)
     print("fname {} -> ".format(fname), end='')
     ctr, sn, s = m.group(1, 2, 3)
     print(ctr, sn, s)
     fname2 = os.path.join(dirName, fname)
     print('\t{} -> {}, {}, {}'.format(fname2, ctr, sn, s))
     tr = np.loadtxt(fname2)
-    dsw.write_next(tr, {'ctr': ctr, 'sn' : sn, 's' : int(s == 'tp')})
+    dsw_tp.write_next(tr, {'ctr': ctr, 'sn' : sn, 's' : int(s == 'tp')})
+
+for dirName, subdirList, fileList in os.walk(fp_dir):
+  print('Processing directory: {}'.format(dirName))
+  for fname in fileList:
+    m = fpre.search(fname)
+    print("fname {} -> ".format(fname), end='')
+    ctr, sn, s = m.group(1, 2, 3)
+    print(ctr, sn, s)
+    fname2 = os.path.join(dirName, fname)
+    print('\t{} -> {}, {}, {}'.format(fname2, ctr, sn, s))
+    tr = np.loadtxt(fname2)
+    dsw_fp.write_next(tr, {'ctr': ctr, 'sn' : sn, 's' : int(s)})
